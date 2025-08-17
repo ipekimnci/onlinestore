37b512c6aa6122cc5464d0659845bd837a2602d5
ipekimnci <ipekimanci@gmail.com>
2025-08-17 22:53:11 +0300

    database configuration changed

--- src/test/java/com/online/onlinestore/OnlineStoreApplicationTests.java
+++ src/test/java/com/online/onlinestore/OnlineStoreApplicationTests.java
@@ -2,12 +2,36 @@ package com.online.onlinestore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
 
+@Testcontainers
 @SpringBootTest
+@ActiveProfiles("test")
 class OnlineStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES =
+            new PostgreSQLContainer<>("postgres:15")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void registerProps(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        r.add("spring.datasource.username", POSTGRES::getUsername);
+        r.add("spring.datasource.password", POSTGRES::getPassword);
+        r.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        r.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        r.add("spring.jpa.properties.hibernate.dialect",
+                () -> "org.hibernate.dialect.PostgreSQLDialect");
+    }
+
+    @Test void contextLoads() {}
 
 }
